30920500a7569f1cdd9a6f7b8d361c5f5ea48943
Dinesh94Singh <dinesh94singh@gmail.com>
2016-12-29 15:39:09 -0500

    14:45 PM - 29/12/16

--- music_content/urls.py
+++ music_content/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
-
 from . import views
 
+
+# namespacing
+app_name = 'music'
+# /music_content/ for first url
+# /music_content/123/ for second url
 urlpatterns = [
-    # /music_content/
     url(r'^$', views.index, name='index'),
-    # /music_content/123/
-    url(r'^(?P<album_id>[0-9]+/)$', views.detail, name='detail'),
-    # homepage for each individual app
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 ]
 
 # what does the above regex say ?
--- music_content/views.py
+++ music_content/views.py
@@ -1,19 +1,22 @@
-from django.http import Http404
 from .models import Album
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 
 def index(request):
     all_albums = Album.objects.all()
     context = {'all_albums': all_albums}
     # context is the information our template needs
-    return render(request, 'music_content/index', context)
+    return render(request, 'music_content/index.html', context)
+
+
+''' try:
+        album = Album.objects.get(pk=album_id)
+    except Exception:
+        raise Http404(" Album not Found !! ") '''
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=int(album_id))
-    except Exception:
-        raise Http404(" Album not Found !! ")
+    album = get_object_or_404(Album, pk=album_id)
     context = {'album': album}
     return render(request, 'music_content/detail.html', context)

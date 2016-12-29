3fedece7851b109dab9d69833c6866dc6aa8b68b
Dinesh94Singh <dinesh94singh@gmail.com>
2016-12-29 17:04:28 -0500

    17:04 29/12/16

--- music_content/urls.py
+++ music_content/urls.py
@@ -2,13 +2,19 @@ from django.conf.urls import url
 from . import views
 
 
-# namespacing
+# name spacing
 app_name = 'music'
+
 # /music_content/ for first url
-# /music_content/123/ for second url
+# /music_content/<album_id>/ for second url
+# /music_content/<album_id>/favorite/
+# third url is for logic filled url
+# (like a url for logout where logout code should happen and another/home page gets loaded)
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
 
 # what does the above regex say ?
--- music_content/views.py
+++ music_content/views.py
@@ -1,4 +1,4 @@
-from .models import Album
+from .models import Album, Song
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
@@ -20,3 +20,20 @@ def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     context = {'album': album}
     return render(request, 'music_content/detail.html', context)
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        context = {
+            'album': album,
+            'error_message': 'You did not select any song, Please select'
+        }
+        return render(request, 'music_content/detail.html', context)
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        context = {'album': album}
+        return render(request, 'music_content/detail.html', context)

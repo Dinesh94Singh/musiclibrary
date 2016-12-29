f9e4efe735c3ff3c06fc792b63cd2fcf9cb68233
Dinesh94Singh <dinesh94singh@gmail.com>
2016-12-29 12:20:26 -0500

    12:19 PM - 29/12/16

---
+++ music_content/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MusicContentConfig(AppConfig):
+    name = 'music_content'
---
+++ music_content/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+# Create your models here.
+class Album(models.Model):
+    artist = models.CharField(max_length=200)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=300)
+    album_logo = models.CharField(max_length=1000)
+
+
+class Song(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
